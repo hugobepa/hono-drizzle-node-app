f8d6498a05ed35badcf2a18825b7ca74250a50c8
hugobepa <hugo_bcn@hotmail.com>
2026-02-08 18:52:54 +0100

    FU_trabajarFichero

--- src/routes/images-route.ts
+++ src/routes/images-route.ts
@@ -10,8 +10,32 @@ imagesRoute.get("/", (c) => {
 imagesRoute.post("/", async (c) => {
   const body = await c.req.parseBody();
   const files = body.image;
-  console.log(files);
-  return c.json({ message: "Hello from images file route!", files });
+
+  // check if files is an array and has length
+  if (!files || (Array.isArray(files) && files.length === 0)) {
+    return c.json({ message: "No files uploaded" }, 400);
+  }
+
+  // if files is not an array, convert it to an array
+  const fileArray = Array.isArray(files) ? files : [files];
+
+  const processedImages = await Promise.all(
+    fileArray.map(async (file) => {
+      // load into a buffer for later use
+      const buffer = Buffer.from(await file.arrayBuffer());
+      return {
+        name: file.name,
+        type: file.type,
+        size: file.size,
+        //buffer,
+      };
+    }),
+  );
+  console.log("files", files, "processedImages", processedImages);
+  return c.json({
+    message: "Hello from images file route!",
+    files: processedImages,
+  });
 });
 
 // export users route
